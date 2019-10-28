bafd4cd523fce180ba83a5dd58105fc9964d6bd3
Celso Milne <celsomilne@gmail.com>
2019-10-29 10:14:06 +1100

    Optional second kwarg to predict

--- src/model/_base.py
+++ src/model/_base.py
@@ -3,7 +3,7 @@ import abc
 class ModelBase(metaclass=abc.ABCMeta):
     
     @abc.abstractmethod
-    def predict(self, X):
+    def predict(self, X, X2=None):
         """
         Abstract predict that must be implemented in subclass
         """
