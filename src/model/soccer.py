6c63744022edacae8f3caf33c4705090c5f00ae6
Celso Milne <celsomilne@gmail.com>
2019-10-29 10:14:31 +1100

    Update soccer.py
    implemented predict and included docstring to explain return type.

--- src/model/soccer.py
+++ src/model/soccer.py
@@ -1,4 +1,28 @@
 from ._base import ModelBase
 
 class SoccerModel(ModelBase):
-    pass
+    
+    def predict(self, frame, prevFrame)  -> list:
+        """Get predicted positions and speeds of players and the ball.
+        
+        Parameters
+        ----------
+        frame : np.ndarray
+            The frame to apply the predicition to.
+        prevFrame : np.ndarray
+            The previous frame in the video. 
+
+        Returns
+        -------
+        prediction : list of tuples
+            Each tuple in `prediction` is (label, (pt1, pt2), speed)
+                * label : str
+                    The label name
+                * pt1 : int
+                    Vertex of the rectangle bounding box
+                * pt2 : int
+                    Vertex of the rectangle opposite to `pt1`
+                * speed : float
+                    Speed in m/s of the label
+        """
+        return
