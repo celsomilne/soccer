258d76caf6e3d4923c33b1fd3d3c78ecd358a0e1
DESKTOP-QGI51VD\jackl <jlie9646@uni.sydney.edu.au>
2019-11-07 23:32:49 +1100

    Commented soccer model file

--- src/model/soccer.py
+++ src/model/soccer.py
@@ -6,17 +6,26 @@ from ._base import ModelBase
 
 
 def merge_similar_lines(l, lines):
+
+	"""
+	Merges line l with set lines - a set of unique lines
+	Adds l to set if it is unique
+	Ignores l if it is contained within another line's neighbourhood
+	Extends unique line with l if l is longer
+	"""
+
+	# Put unique lines in array if theres only 1, for looping
     if len(lines.shape) == 1:
         lines = np.array([lines])
 
-    # Translate to similar line bounds
+    # Translate line to align with each unique line
     d = np.column_stack(
         ((lines[:, 0] + lines[:, 2]) / 2, (lines[:, 1] + lines[:, 3]) / 2)
     )
     d = np.tile(d, (1, 2))
     tl = l - d
 
-    # Rotate line to similar line bounds
+    # Rotate line to align with each unique line
     xd = lines[:, 2] - lines[:, 0]
     yd = lines[:, 3] - lines[:, 1]
     td = np.sqrt(xd * xd + yd * yd)
@@ -37,27 +46,36 @@ def merge_similar_lines(l, lines):
         + 10
     )
     yb = 15
-
+	
+	# Check if line is similar to any unique line
     similar = np.logical_and(abs(tl[:, 1]) < yb, abs(tl[:, 3]) < yb)
     if sum(similar) > 1:
+		# If multiple similar lines, take the most similar
         diffs = np.maximum(abs(tl[:, 1]), abs(tl[:, 3]))
         similar[:] = False
         similar[np.argmin(diffs)] = True
 
     if any(similar):
+		# If line is similar, check if it extends beyond current unique line
+		# Update unique line to new length if it does
         xb = xb[similar]
         if tl[similar, 0] < -xb or tl[similar, 0] > xb:
             lines[similar, 0:2] = l[0:2]
         if tl[similar, 2] < -xb or tl[similar, 2] > xb:
             lines[similar, 2:4] = l[2:4]
     else:
+		# If line is sufficiently differet than other unique lines, add to unique set
         lines = np.concatenate((lines, l.reshape(1, 4)), axis=0)
 
     return lines
 
 
 def is_edge_line(l, dims):
+
+	# Tolerance for line to be considered on the edge
     tol = 10
+	
+	# Check if line is on any 4 borders
     bad = (
         (l[0] < tol and l[2] < tol)
         or (l[1] < tol and l[3] < tol)
@@ -68,15 +86,20 @@ def is_edge_line(l, dims):
 
 
 def find_intersection(l1, l2):
+
+	# Calculate intercept and gradient of first line
     m1 = (l1[3] - l1[1]) / (l1[2] - l1[0])
     b1 = l1[1] - m1 * l1[0]
-
+	
+	# If line is vertical, manually derive intersection
     if l2[0] == l2[2]:
         return np.array([l2[0], m1 * l2[0] + b1])
-
+	
+	# Find intercept and gradient of second line
     m2 = (l2[3] - l2[1]) / (l2[2] - l2[0])
     b2 = l2[1] - m2 * l2[0]
-
+	
+	# Calculate intercepts of lines
     x = (b2 - b1) / (m1 - m2)
     y = m1 * x + b1
 
@@ -84,20 +107,28 @@ def find_intersection(l1, l2):
 
 
 def generate_points(line_dict, right_view, true_dict, rel_dict):
-
+	
+	# initialise vector/matrix of coefficients
     A = np.array([]).reshape(0, 8)
     b = np.array([])
+	
+	# Loop through each pair of potential line intercepts to check if they've been detected 
     for n1 in rel_dict.keys():
         if n1 in line_dict:
             for n2 in rel_dict[n1]:
                 if n2 in line_dict:
+					# Calculate line intercepts on field
                     x = np.array([0.0, 0.0])
                     x += true_dict[n1]
                     if right_view:
                         x += true_dict[n2]
                     else:
                         x -= true_dict[n2]
+						
+					# Calculate line intercepts in frame
                     u = find_intersection(line_dict[n1], line_dict[n2])
+					
+					# Add results to coefficient matrix/vector
                     A = np.vstack(
                         (
                             A,
@@ -108,28 +139,16 @@ def generate_points(line_dict, right_view, true_dict, rel_dict):
                         )
                     )
                     b = np.concatenate((b, x))
-
+	
+	# Add additional 'half' points to straighten far side line if in centre field case
     if right_view == 1:
         u = find_intersection(line_dict["top"], line_dict["left"])
         A = np.vstack((A, [[0, 0, 0, u[0], u[1], 1, -70 * u[0], -70 * u[1]]]))
         u = find_intersection(line_dict["top"], line_dict["right"])
         A = np.vstack((A, [[0, 0, 0, u[0], u[1], 1, -70 * u[0], -70 * u[1]]]))
         b = np.concatenate((b, [70, 70]))
-        """
-    elif right_view == 0:
-        u = find_intersection(line_dict['top'],line_dict['right'])
-        A = np.vstack((A,[[0,0,0,u[0],u[1],1,-70*u[0],-70*u[1]]]))
-        u = find_intersection(line_dict['goal'],line_dict['left'])
-        A = np.vstack((A,[[u[0],u[1],1,0,0,0,50*u[0],50*u[1]])
-        b = np.concatenate((b,[70,-50]))
-    elif right_view == 2:
-        u = find_intersection(line_dict['top'],line_dict['left'])
-        A = np.vstack((A,[[0,0,0,u[0],u[1],1,-70*u[0],-70*u[1]]]))
-        u = find_intersection(line_dict['goal'],line_dict['right'])
-        A = np.vstack((A,[[u[0],u[1],1,0,0,0,-50*u[0],-50*u[1]])
-        b = np.concatenate((b,[70,50]))
-        """
-
+	
+	# Calculate homographic transform matrix
     c, _, _, _ = np.linalg.lstsq(A, b, rcond=None)
     c = np.concatenate((c, np.array([1.0])))
     c = c.reshape(3, 3)
@@ -138,6 +157,8 @@ def generate_points(line_dict, right_view, true_dict, rel_dict):
 
 
 class FieldTransform:
+
+	# Initialise the location of the lines on soccer field
     true_dict = {
         'top': np.array([0.,70.]).astype(np.float32),
         'mid': np.array([0.,0.]).astype(np.float32),
@@ -151,7 +172,8 @@ class FieldTransform:
         'lcirc': np.array([-9.15,0.]).astype(np.float32),
         'ccirc': np.array([0.,35.]).astype(np.float32)
     }
-
+	
+	# Initialise dictionary of line intercepts
     rel_dict = {
         'top': ['mid','box','goal'],
         'tbox': ['box','goal'],
@@ -162,54 +184,66 @@ class FieldTransform:
     }
 
     def __call__(self, im):
+	
+		# Creake a mask to extract the field
         mask_field = np.logical_and(im[:,:,1] > im[:,:,0], im[:,:,1] > im[:,:,2]).astype(np.uint8)
-
+		
+		# Use morphological operations to clear noise from th mask
         k = 40
         kernel = np.ones((k,k),np.uint8)
         mask = cv2.morphologyEx(mask_field, cv2.MORPH_OPEN, kernel)
         kernel = np.ones((2*k,2*k),np.uint8)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
+		
+		# Apply mask back on to image to get field
         field = cv2.bitwise_and(im, im, mask = mask)
-
+		
+		# Mildly decay mask to remove edges generated by mask edge
         kernel = np.ones((1,1),np.uint8)
         mask = cv2.erode(mask,kernel,iterations = 1)
-
+		
+		# Use blue channel to extract edged as maximally exposes soccer lines
         gray = im[:,:,2]
-
+		
+		# Apply Canny edge detection technique
         blur_gray = cv2.GaussianBlur(gray,(5, 5),0)
-
         low_threshold = 30
         high_threshold = 200
         edges = cv2.Canny(gray, low_threshold, high_threshold)
         edges = cv2.bitwise_and(edges, edges, mask = mask)
         edges = cv2.dilate(edges, np.ones((3,3)))
-
+		
+		# Find lines in image using probabilistic Hough transform
         rho_tol = 1
         theta_tol = np.pi/180
         lines = np.array(cv2.HoughLinesP(edges,rho_tol,theta_tol,50,minLineLength=100,maxLineGap=5))
         lines.shape = (lines.shape[0], lines.shape[2])
-
-        line_im = im.copy()
-
+		
+		# Remove lines on border and merge remaining multiple detections
         good_lines = np.array([l for l in lines if not is_edge_line(l,edges.shape)])
-
         lines = good_lines[0]
         for i in range(1,len(good_lines)):
             lines = merge_similar_lines(good_lines[i],lines)
-
+		
+		# Direction of each line
         theta = np.arctan2(lines[:,3]-lines[:,1], lines[:,2]-lines[:,0])%np.pi
-
+		
+		# Initialise dictionary to store classified lines
         line_dict = {'left': np.array([0,0,0,edges.shape[0]]),
                     'right': np.array([edges.shape[1],0,edges.shape[1],edges.shape[0]])}
         right_view = 2
-
+		
+		# Check if near vertical line in image (centre field indicator)
         if np.any(abs(theta - np.pi/2) < np.pi/60)  or len(theta) < 2:
+			# Update view flag for centre field
             right_view = 1
+			
+			#Classify halfway line and far sideline as most and horizontal lines respectively 
             line_dict['mid'] = lines[np.argmin(abs(theta-np.pi/2))]
             line_dict['top'] = lines[np.pi/2 - abs(theta - np.pi/2) < np.pi/30][0]
             midpoint = (line_dict['mid'][0] + line_dict['mid'][2])//2
-
+			
+			# Remove lines and boarder from the edge output
             line_mask = np.ones(gray.shape, np.uint8)
             for x1,y1,x2,y2 in line_dict.values():
                 cv2.line(line_mask,(x1,y1), (x2,y2), (0,0,0),10)
@@ -217,56 +251,75 @@ class FieldTransform:
             line_mask[(line_mask.shape[0] - line_mask.shape[0]//8):line_mask.shape[0],:] = 0
             line_mask[:,:20] = 0
             line_mask[:,line_mask.shape[1]-20:] = 0
-
             edges = cv2.bitwise_and(edges, edges, mask = line_mask)
             
+			# Find the largest remaining contour in image (centre circle)
             conts, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             c = max(conts, key = cv2.contourArea)
             x,y,w,h = cv2.boundingRect(c)
             
+			# Find closest remaining lines to the halfway line
+			# (these lines form the edges of the ellipse)
             line_disp = lines[:,[0,3]] - midpoint
             idx = np.argpartition(np.mean(line_disp,axis=1) + np.min(line_disp,axis=1), 2)
             lines = lines[np.any(lines != line_dict['top'],axis=1)]
             lines = lines[np.any(lines != line_dict['mid'],axis=1)]
-
             lines = lines[idx[0:2]]
+			
+			# Classify these lines as centre circle intercept with the halfway line
             line_dict['tcirc'] = lines[np.argmin(np.mean(lines[:,[1,3]],axis=1))]
             line_dict['bcirc'] = lines[np.argmax(np.mean(lines[:,[1,3]],axis=1))]
-
+			
+			# Find halfway point between this intersection
             c_top = find_intersection(line_dict['tcirc'], line_dict['mid'])
             c_bot = find_intersection(line_dict['bcirc'], line_dict['mid'])
             c_cen = (c_top[1] + c_bot[1])/2
             line_dict['ccirc'] = np.array([x,c_cen,x+w,c_cen])
+			
+			# From largest contour, determine whether it lies to left or right of halfway line
+			# Classify an extreme point of centre circle accordingly
             if x < midpoint:
                 line_dict['lcirc'] = np.array([x,y,x,y+h])
             else:
                 line_dict['rcirc'] = np.array([x+w,y,x+w,y+h])
-        else:
+        
+		# No halfway line (end field indicator)
+		else:
+		
+			# Classify horizontal lines on field as minimum line orientation
             horz_theta = theta[np.argmin(np.pi/2-abs(theta-np.pi/2))]
-
+			
+			# Determine if at left or right end of field based on horizontal orientation
             if horz_theta < np.pi/2:
                 right_view = 0
             
+			#Extract all horizontal lines in image
             horz_lines = lines[abs(theta-horz_theta) < np.pi/30]
+			
+			# Classify far sideline as heightest horizontal line and remove from sets
             line_dict['top'] = horz_lines[np.argmin(np.minimum(horz_lines[:,1], horz_lines[:,3]))]
             lines = lines[np.any(lines != line_dict['top'],axis=1)]
             horz_lines = horz_lines[np.any(horz_lines != line_dict['top'],axis=1)]
-
+			
+			# Check if goal line in image by comparing endpoints with far sideline
             if right_view:
                 goal_line_check = np.logical_and(lines[:,0]-line_dict['top'][2]<100,\
                                             lines[:,1]-line_dict['top'][3]<20)
             else:
                 goal_line_check = np.logical_and(lines[:,2]-line_dict['top'][0]<100,\
                                             lines[:,3]-line_dict['top'][1]<20)
-
+			
+			# Classify any positive hits as the goal line
             if any(goal_line_check):
                 line_dict['goal'] = lines[goal_line_check][0]
                 lines = lines[np.logical_not(goal_line_check)]
-
+			
+			# Classify far pentaly box line as next heighest horizontal line and remove
             line_dict['tbox'] = horz_lines[np.argmin(horz_lines[:,1]+horz_lines[:,3])]
             lines = lines[np.any(lines != line_dict['tbox'],axis=1)]
             horz_lines = horz_lines[np.any(horz_lines != line_dict['tbox'],axis=1)]
 
+			# Check if any horizontal lines remain and classify next heightest as close penalty box line
             if len(horz_lines) != 0 and 'tbox' in line_dict:
                 bbox_temp = horz_lines[np.argmin(horz_lines[:,1]+horz_lines[:,3])]
                 if np.max(bbox_temp[[1,3]]) - np.max(line_dict['tbox'][[1,3]]) > \
@@ -274,15 +327,19 @@ class FieldTransform:
                     line_dict['bbox'] = bbox_temp            
                     lines = lines[np.any(lines != line_dict['bbox'],axis=1)]
                     horz_lines = horz_lines[np.any(horz_lines != line_dict['bbox'],axis=1)]
-
+					
+			# Check if vertical pentaly box line in image by comparing endpoints
             if right_view:
                 box_line_check = np.linalg.norm(lines[:,0:2]-line_dict['tbox'][0:2],axis=1) < 50
             else:
                 box_line_check = np.linalg.norm(lines[:,2:4]-line_dict['tbox'][2:4],axis=1) < 50
+				
+			# Classify any positive hits as the vertical penalty box line
             if any(box_line_check):
                 line_dict['box'] = lines[box_line_check][0]
                 lines = lines[np.logical_not(box_line_check)]
-
+		
+		# Generate transform using the classified lines from the image
         c = generate_points(line_dict, right_view, self.true_dict, self.rel_dict)
 
         return c
@@ -310,6 +367,7 @@ class SoccerModel:
         height = obj["height"]
 
         # Get only rows for which the width and height are within 2 standard deviations
+		# (Extracts players)
         obj = obj[np.abs(width - np.mean(width)) / np.std(width) < 2]
         obj = obj[np.abs(height - np.mean(height)) / np.std(height) < 2]
 
@@ -317,11 +375,11 @@ class SoccerModel:
         obj["ufeet"] = obj["left"] + obj["width"] / 2
         obj["vfeet"] = obj["top"] + obj["height"]
 
-        # Get the right and bottom
+        # Get the right and bottom bounding boxes
         obj["right"] = obj["left"] + obj["width"]
         obj["bottom"] = obj["top"] + obj["height"]
 
-        # Get the feature transform
+        # Get the feature transform if available and not corrupt
         try:
             c = self.ft(frame)
             if (np.abs(c[0,2]) > .01 and np.abs(c[1,2]) > .01 and self.count > 5) or frameIdx == 1:
@@ -331,10 +389,8 @@ class SoccerModel:
             pass
         finally:
             self.count += 1
-    
-        if frameIdx == 200:
-            c = self.ft(frame)
-
+		
+		# Stack feet coordinates to be transformed
         u = np.vstack(
             (obj["ufeet"].values, obj["vfeet"].values, np.ones((1, len(obj))))
         )
@@ -354,7 +410,7 @@ class SoccerModel:
 
         if frameIdx == 1:
 
-            # Store and abort
+            # Store data and abort
             obj["vel"] = 0.0
             self.obj_old = obj
         else:
@@ -381,7 +437,7 @@ class SoccerModel:
             # Maximum euclidean distance between matching players
             tol = 50
 
-            # Find the minimum between (tol, min(diffs))
+            # Iterate until tolerance met or no objects in frame
             while np.any(diffs < tol) and len(obj) > 0 and len(self.obj_old) > 0:
                 ind = np.unravel_index(np.argmin(diffs, axis=None), diffs.shape)
                 obj.loc[ind[1], ["xold", "yold"]] = xy_old[ind[0], 0:2]
@@ -403,9 +459,11 @@ class SoccerModel:
 
             # Cap speed at 10
             obj.loc[obj['vel'] > 10, 'vel'] = 10
-
+		
+		# Store new objects for next frame
         self.obj_old = obj
-
+		
+		# Format data for display functions
         vals = [
             (
                 i["label"],
